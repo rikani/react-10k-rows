fef6d1b037460c7458b9fd0ea6972e6e0b73629d
Alexander Bulyga <abulyga@wisebits.com>
2019-05-14 16:28:39 +0300

    Implement sticky filter

--- frontend/js/components/app.js
+++ frontend/js/components/app.js
@@ -3,20 +3,56 @@ import Header from './header.js'
 import Filters from './filters.js'
 import Stack from './stack.js'
 
-const App = () => (
-  <div className="app">
-    <header className="app__header">
-      <Header title="Stack" />
-    </header>
-    <div className="app__body">
-      <aside className="app__aside">
-        <Filters />
-      </aside>
-      <section className="app__main">
-        <Stack height={40} />
-      </section>
-    </div>
-  </div>
-)
+const BODY_STICKY_CLASS = 'app__body_sticky'
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.body = React.createRef()
+
+    this.onWindowScroll = this.onWindowScroll.bind(this)
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.onWindowScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onWindowScroll)
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <header className="app__header">
+          <Header title="Stack" />
+        </header>
+        <div className="app__body" ref={this.body}>
+          <aside className="app__aside">
+            <Filters />
+          </aside>
+          <section className="app__main">
+            <Stack height={40} />
+          </section>
+        </div>
+      </div>
+    )
+  }
+
+  onWindowScroll() {
+    this.toggleStickyMode()
+  }
+
+  toggleStickyMode() {
+    const body = this.body.current
+    const { top } = body.getBoundingClientRect()
+
+    if (top <= 0) {
+      body.classList.add(BODY_STICKY_CLASS)
+    } else {
+      body.classList.remove(BODY_STICKY_CLASS)
+    }
+  }
+}
 
 export default App
